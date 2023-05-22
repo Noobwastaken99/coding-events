882344b38899303dd7d59dba4c0c97a9b536df46
Noobwastaken99 <czmoni99@yahoo.com>
2023-05-22 12:36:56 -0500

    Finally got @Entity to work and change implementation 'mysql:mysql-connector-java' to implementation 'com.mysql:mysql-connector-j'

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,6 +1,6 @@
 package org.launchcode.codingevents.models;
 
-//import javax.persistence.Entity;
+import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
