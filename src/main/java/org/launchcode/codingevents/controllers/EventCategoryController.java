128ab5118f98f7c9c13146e17836057b22086a2e
Noobwastaken99 <czmoni99@yahoo.com>
2023-05-22 16:00:53 -0500

    exercise 17 should be done, but eventCategoryRepository.save(newEventCategory); has an issue

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -28,13 +28,15 @@ public class EventCategoryController {
     }
 
     @PostMapping("create")
-    public String processCreateEventCategoryForm(@Valid @ModelAttribute EventCategory eventCategory, Errors errors, Model model) {
+    public String processCreateEventCategoryForm(@Valid @ModelAttribute EventCategory newEventCategory, Errors errors, Model model) {
+
         if (errors.hasErrors()) {
             model.addAttribute("title", "Create Category");
-            model.addAttribute(new EventCategory());
+           model.addAttribute(new EventCategory());
             return "eventCategories/create";
         }
-        eventCategoryRepository.save(eventCategory);
+
+        eventCategoryRepository.save(newEventCategory);
         return "redirect:";
     }
 
